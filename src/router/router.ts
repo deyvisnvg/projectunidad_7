4e907413b55bcb5ab56db20e5270d06e8c82c62d
deyvisnvg <deyvisnvg@gmail.com>
2023-01-20 14:39:01 -0500

    se definió el type de 'app' en el import src/router

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,7 +1,8 @@
+import { type Application } from "express";
 import userRouter from "../components/user";
 import songRouter from "../components/songs";
 
-export default (app: any) => {
+export default (app: Application) => {
     app.use("/api/v1/users", userRouter);
     app.use("/api/v1/songs", songRouter);
 }
